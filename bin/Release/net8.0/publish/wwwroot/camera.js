156bfaf393d47e64b455c1e780d937b16ed80957
aditijaiswal2 <157448453+aditijaiswal2@users.noreply.github.com>
2024-07-24 13:14:58 +0530

    Add files via upload

---
+++ bin/Release/net8.0/publish/wwwroot/camera.js
@@ -0,0 +1,24 @@
+﻿async function startCamera(facingMode) {
+    const constraints = {
+        video: {
+            facingMode: facingMode
+        }
+    };
+
+    try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        const videoElement = document.getElementById('video');
+        videoElement.srcObject = stream;
+        videoElement.play();
+    } catch (error) {
+        console.error("Error accessing camera: ", error);
+    }
+}
+
+function startFrontCamera() {
+    startCamera("user");
+}
+
+function startBackCamera() {
+    startCamera("environment");
+}
